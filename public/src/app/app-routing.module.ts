014d43c0f2132248165f05ea82b74a0ae2401a6c
Leo Lai <38085012+leolai2010@users.noreply.github.com>
2018-10-10 13:20:42 -0700

    fixed front end styling

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
-  { path: '', pathMatch: 'full', redirectTo: '/landing' },
-  { path: '**', pathMatch: 'full', redirectTo: '/landing' }
+  { path: 'home', component: HomeComponent},
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: '**', pathMatch: 'full', redirectTo: '/home' }
 ];
 
 @NgModule({
